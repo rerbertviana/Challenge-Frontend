5e49605a18c65ab2e72074d73fd853e2e820612a
rerbert viana <rerbertviana@gmail.com>
2021-12-24 11:56:25 -0500

    add Dialog

--- src/components/content/Search.vue
+++ src/components/content/Search.vue
@@ -11,7 +11,8 @@
                 loading
             >
             <template v-slot:[`item.actions`]="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+                <v-avatar size="30" style="background-color:#9EADBA; border-radius:100%;" ><v-icon small class="mr-2" @click="editItem(item)" style="color:white;"> mdi-pencil </v-icon></v-avatar>
+                <v-avatar size="30" style="background-color:#9EADBA; border-radius:100%; margin-left:10px;" ><v-icon small class="mr-2" @click="editItem(item)" style="color:white;"> mdi-delete </v-icon></v-avatar>
             </template>
             </v-data-table>
         </div>
@@ -19,19 +20,35 @@
         <div>
             {{editedItem.name.first}}
         </div>
-        <el-button type="text" @click="dialogVisible = true">click to open the Dialog</el-button>
-
-<el-dialog
-  title="Tips"
-  :visible.sync="dialogVisible"
-  width="30%"
-  :before-close="handleClose">
-  <span>This is a message</span>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="dialogVisible = false">Cancel</el-button>
-    <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
-  </span>
-</el-dialog>
+        <el-dialog :visible.sync="dialogVisible" width="50%" >
+            <v-row>
+                <v-col style="display:flex; align-items:center;">
+                    <span style="margin-right:10px; width:100px;">Name:</span>
+                    <el-input placeholder="Title" v-model="editedItem.name.title" clearable style="width:170px; margin-right:10px;"></el-input>
+                    <el-input placeholder="First" v-model="editedItem.name.first" clearable style="width:170px; margin-right:10px;"></el-input>
+                    <el-input placeholder="Last" v-model="editedItem.name.last" clearable style="width:170px;"></el-input>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col style="display:flex; align-items:center;">
+                    <span style="margin-right:20px; width:100px;">Email:</span>
+                    <el-input placeholder="Email" v-model="editedItem.email" clearable ></el-input>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col style="display:flex; align-items:center;">
+                    <span style="margin-right:20px; width:100px;">Gender:</span>
+                    <el-input placeholder="Email" v-model="editedItem.gender" clearable ></el-input>
+                    <span style="margin-right:20px; width:100px;">Bir</span>
+                    <el-input placeholder="Email" v-model="editedItem.gender" clearable ></el-input>
+                </v-col>
+            </v-row>
+            
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">Cancel</el-button>
+            <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+        </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -83,15 +100,9 @@ export default {
         editItem (item) {
             this.editedIndex = this.users.indexOf(item)
             this.editedItem = Object.assign({}, item)
+            this.dialogVisible = true;
         },
 
-        handleClose(done) {
-        this.$confirm('Are you sure to close this dialog?')
-          .then(()=> {
-            done();
-          })
-          .catch(()=> {});
-      }
     },
 
     mounted() {
