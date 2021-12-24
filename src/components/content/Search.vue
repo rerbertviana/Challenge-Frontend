f0e791f5774d678037d723857c3e6095878ae195
Rerbert Viana <rerbertviana@gmail.com>
2021-12-23 22:03:17 -0500

    add dialog

--- src/components/content/Search.vue
+++ src/components/content/Search.vue
@@ -19,6 +19,19 @@
         <div>
             {{editedItem.name.first}}
         </div>
+        <el-button type="text" @click="dialogVisible = true">click to open the Dialog</el-button>
+
+<el-dialog
+  title="Tips"
+  :visible.sync="dialogVisible"
+  width="30%"
+  :before-close="handleClose">
+  <span>This is a message</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">Cancel</el-button>
+    <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+  </span>
+</el-dialog>
     </div>
 </template>
 
@@ -35,6 +48,7 @@ export default {
         return {
             users: [],
             search: '',
+            dialogVisible: false,
             headers: [
                 {
                     text: 'Name (100g serving)',
@@ -70,6 +84,14 @@ export default {
             this.editedIndex = this.users.indexOf(item)
             this.editedItem = Object.assign({}, item)
         },
+
+        handleClose(done) {
+        this.$confirm('Are you sure to close this dialog?')
+          .then(()=> {
+            done();
+          })
+          .catch(()=> {});
+      }
     },
 
     mounted() {
